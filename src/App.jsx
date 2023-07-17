33514a12a6ef619fe541dad9665b6fc1f0cf0359
Dibyendu Das <get2dibyendu@gmail.com>
2023-07-17 11:04:42 +0530

    minimal changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,18 @@ import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Cart from "./pages/Cart";
 import { Navigate, Route, Router, Routes, redirect, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 
 function App() {
   const navigate = useNavigate();
-  const user = true;
+  const user = useSelector((state) => state.user.currentUser);
 
   return (
     <Routes>
       <Route path="/" element={<Home/>} />
-      <Route path="/login" element={<Login/>} />
-      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login user={user}/>} />
+      <Route path="/register" element={<Register user={user}/>} />
       <Route path="/products/:category" element={<ProductList/>}/>
       <Route path="/product/:id" element={<Product/>}/>
       <Route path="/cart" element={<Cart/>} />
